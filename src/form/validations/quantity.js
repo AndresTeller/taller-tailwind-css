f60ac8cac267f1ef1af54699231031a0ba26c3bd
Andres Teller <andres.teller2@gmail.com>
2023-03-10 23:01:28 -0500

    Agregando validación en quantity.js

--- src/form/validations/quantity.js
+++ src/form/validations/quantity.js
@@ -1,5 +1,7 @@
 import { isCamp, isNumber, isPositiveNumber } from "./utility.js";
 
+const MAX_QUANTITY = 30;
+
 export const validateQuantity = (quantity = 0) => {
   if (!isCamp(quantity)) return "Por favor ingrese una cantidad.";
 
@@ -7,6 +9,9 @@ export const validateQuantity = (quantity = 0) => {
 
   if (!isPositiveNumber(quantity))
     return "Por favor, ingrese sólo números positivos.";
+
+  if (quantity > MAX_QUANTITY)
+    return `La cantidad máxima de productos a escoger es ${MAX_QUANTITY}`;
 
   return true;
 };
