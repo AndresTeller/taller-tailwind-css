5685b05cc5e8a741d8fbfd548a633f481505fde3
Andres Teller <andres.teller2@gmail.com>
2023-03-27 12:53:39 -0500

    Cambio de base de datos de remoto a local en db.js

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,11 +1,19 @@
 const { createPool } = require("mysql2/promise");
 
 const pool = createPool({
+  host: "localhost",
+  user: "root",
+  password: "123456789",
+  port: "3306",
+  database: "desarrollo_web",
+});
+
+module.exports = { pool };
+
+/*
   host: "containers-us-west-195.railway.app",
   user: "root",
   password: "FpK6pnquTByNn3NNmRQD",
   port: "5943",
   database: "railway",
-});
-
-module.exports = { pool };
+*/
