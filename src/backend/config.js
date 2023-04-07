9dff0a5d36c1c831e83c4a72404af3d2c7aa2b76
Andres Teller <andres.teller2@gmail.com>
2023-04-06 23:38:26 -0500

    Eliminación de console logs de prueba.

--- src/backend/config.js
+++ src/backend/config.js
@@ -7,20 +7,6 @@ const PORT = process.env.PORT || 5000,
   DB_PORT = process.env.DB_PORT || 3306,
   DB_NAME = process.env.DB_NAME || "desarrollo_web";
 
-console.log(PORT);
-console.log(DB_HOST);
-console.log(DB_NAME);
-console.log(DB_PASSWORD);
-console.log(DB_PORT);
-console.log(DB_NAME);
-
-console.log(process.env.PORT);
-console.log(process.env.DB_HOST);
-console.log(process.env.DB_USER);
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_PORT);
-console.log(process.env.DB_NAME);
-
 module.exports = {
   PORT,
   DB_HOST,
